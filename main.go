8fa93608bb6cbfa96c8610bc2c03aa519af1129b
nicktej <tann@lclark.edu>
2019-07-28 13:54:08 -0700

    added lives, random ghost movements, goroutines

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"os/exec"
+	"time"
 )
 
 // Player is the player character \o/
@@ -15,27 +17,14 @@ type Player struct {
 }
 
 var player Player
-var maze []string
 
-func init() {
-	cbTerm := exec.Command("/bin/stty", "cbreak", "-echo")
-	cbTerm.Stdin = os.Stdin
-
-	err := cbTerm.Run()
-	if err != nil {
-		log.Fatalf("Unable to activate cbreak mode terminal: %v\n", err)
-	}
+// Ghost is the enemy that chases the player :O
+type Ghost struct {
+	row int
+	col int
 }
 
-func cleanup() {
-	cookedTerm := exec.Command("/bin/stty", "-cbreak", "echo")
-	cookedTerm.Stdin = os.Stdin
-
-	err := cookedTerm.Run()
-	if err != nil {
-		log.Fatalf("Unable to activate cooked mode terminal: %v\n", err)
-	}
-}
+var ghosts []*Ghost
 
 func loadMaze() error {
 	f, err := os.Open("maze01.txt")
@@ -55,11 +44,57 @@ func loadMaze() error {
 			switch char {
 			case 'P':
 				player = Player{row, col}
+			case 'G':
+				ghosts = append(ghosts, &Ghost{row, col})
+			case '.':
+				numDots++
 			}
 		}
 	}
 
 	return nil
+}
+
+var maze []string
+var score int
+var numDots int
+var lives = 1
+
+func clearScreen() {
+	fmt.Printf("\x1b[2J")
+	moveCursor(0, 0)
+}
+
+func moveCursor(row, col int) {
+	fmt.Printf("\x1b[%d;%df", row+1, col+1)
+}
+
+func printScreen() {
+	clearScreen()
+	for _, line := range maze {
+		for _, chr := range line {
+			switch chr {
+			case '#':
+				fallthrough
+			case '.':
+				fmt.Printf("%c", chr)
+			default:
+				fmt.Printf(" ")
+			}
+		}
+		fmt.Printf("\n")
+	}
+
+	moveCursor(player.row, player.col)
+	fmt.Printf("P")
+
+	for _, g := range ghosts {
+		moveCursor(g.row, g.col)
+		fmt.Printf("G")
+	}
+
+	moveCursor(len(maze)+1, 0)
+	fmt.Printf("Score: %v\tLives: %v\n", score, lives)
 }
 
 func readInput() (string, error) {
@@ -126,37 +161,56 @@ func makeMove(oldRow, oldCol int, dir string) (newRow, newCol int) {
 
 func movePlayer(dir string) {
 	player.row, player.col = makeMove(player.row, player.col, dir)
-}
-
-func moveCursor(row, col int) {
-	fmt.Printf("\x1b[%d;%df", row+1, col+1)
-}
-
-func clearScreen() {
-	fmt.Printf("\x1b[2J")
-	moveCursor(0, 0)
-}
-
-func printScreen() {
-	clearScreen()
-	for _, line := range maze {
-		for _, chr := range line {
-			switch chr {
-			case '#':
-				fmt.Printf("%c", chr)
-			default:
-				fmt.Printf(" ")
-			}
-		}
-		fmt.Printf("\n")
+	switch maze[player.row][player.col] {
+	case '.':
+		numDots--
+		score++
+		// Remove dot from the maze
+		maze[player.row] = maze[player.row][0:player.col] + " " + maze[player.row][player.col+1:]
 	}
+}
 
-	moveCursor(player.row, player.col)
-	fmt.Printf("P")
+func drawDirection() string {
+	dir := rand.Intn(4)
+	move := map[int]string{
+		0: "UP",
+		1: "DOWN",
+		2: "RIGHT",
+		3: "LEFT",
+	}
+	return move[dir]
+}
+
+func moveGhosts() {
+	for _, g := range ghosts {
+		dir := drawDirection()
+		g.row, g.col = makeMove(g.row, g.col, dir)
+	}
+}
+
+func initialize() {
+	cbTerm := exec.Command("/bin/stty", "cbreak", "-echo")
+	cbTerm.Stdin = os.Stdin
+
+	err := cbTerm.Run()
+	if err != nil {
+		log.Fatalf("Unable to activate cbreak mode terminal: %v\n", err)
+	}
+}
+
+func cleanup() {
+	cookedTerm := exec.Command("/bin/stty", "-cbreak", "echo")
+	cookedTerm.Stdin = os.Stdin
+
+	err := cookedTerm.Run()
+	if err != nil {
+		log.Fatalf("Unable to activate cooked mode terminal: %v\n", err)
+	}
 }
 
 func main() {
 	// initialize game
+	initialize()
 	defer cleanup()
 
 	// load resources
@@ -165,29 +219,50 @@ func main() {
 		log.Printf("Error loading maze: %v\n", err)
 		return
 	}
+
+	// process input (async)
+	input := make(chan string)
+	go func(ch chan<- string) {
+		for {
+			input, err := readInput()
+			if err != nil {
+				log.Printf("Error reading input: %v", err)
+				ch <- "ESC"
+			}
+			ch <- input
+		}
+	}(input)
+
 	// game loop
 	for {
+		// process movement
+		select {
+		case inp := <-input:
+			if inp == "ESC" {
+				lives = 0
+			}
+			movePlayer(inp)
+		default:
+		}
+
+		moveGhosts()
+
+		// process collisions
+		for _, g := range ghosts {
+			if player.row == g.row && player.col == g.col {
+				lives = 0
+			}
+		}
+
 		// update screen
 		printScreen()
 
-		// process input
-		input, err := readInput()
-		if err != nil {
-			log.Printf("Error reading input: %v", err)
-			break
-		}
-
-		// process movement
-		movePlayer(input)
-
-		// process collisions
-
 		// check game over
-		if input == "ESC" {
+		if numDots == 0 || lives == 0 {
 			break
 		}
 
 		// repeat
+		time.Sleep(200 * time.Millisecond)
 	}
-
 }
