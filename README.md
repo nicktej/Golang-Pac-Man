83a4f54c4fbd35f0a43cd3386feca9ab6c2a5017
nicktej <tann@lclark.edu>
2019-07-28 11:40:15 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Golang Pac-Man
+
+My attempt at learning Golang by building a Pac-Man game!
---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+)
+
+var maze []string
+
+func init() {
+	cbTerm := exec.Command("/bin/stty", "cbreak", "-echo")
+	cbTerm.Stdin = os.Stdin
+
+	err := cbTerm.Run()
+	if err != nil {
+		log.Fatalf("Unable to activate cbreak mode terminal: %v\n", err)
+	}
+}
+
+func cleanup() {
+	cookedTerm := exec.Command("/bin/stty", "-cbreak", "echo")
+	cookedTerm.Stdin = os.Stdin
+
+	err := cookedTerm.Run()
+	if err != nil {
+		log.Fatalf("Unable to activate cooked mode terminal: %v\n", err)
+	}
+}
+
+func loadMaze() error {
+	f, err := os.Open("maze01.txt")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		maze = append(maze, line)
+	}
+
+	return nil
+}
+
+func readInput() (string, error) {
+	buffer := make([]byte, 100)
+
+	cnt, err := os.Stdin.Read(buffer)
+	if err != nil {
+		return "", err
+	}
+
+	if cnt == 1 && buffer[0] == 0x1b {
+		return "ESC", nil
+	}
+
+	return "", nil
+}
+
+func moveCursor(row, col int) {
+	fmt.Printf("\x1b[%d;%df", row+1, col+1)
+}
+
+func clearScreen() {
+	fmt.Printf("\x1b[2J")
+	moveCursor(0, 0)
+}
+
+func printScreen() {
+	clearScreen()
+	for _, line := range maze {
+		fmt.Println(line)
+	}
+}
+
+func main() {
+	// initialize game
+	defer cleanup()
+
+	// load resources
+	err := loadMaze()
+	if err != nil {
+		log.Printf("Error loading maze: %v\n", err)
+		return
+	}
+	// game loop
+	for {
+		// update screen
+		printScreen()
+		// process input
+		input, err := readInput()
+		if err != nil {
+			log.Printf("Error reading input: %v", err)
+			break
+		}
+		if input == "ESC" {
+			break
+		}
+
+		// process movement
+
+		// process collisions
+
+		// check game over
+
+		// Temp: break infinite loop
+		fmt.Println("Hello, Pac Go!")
+		break
+
+		// repeat
+	}
+}
